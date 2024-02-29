59d2ca0fac6ca94e66c07841a075303c086e8771
MilosTanaskovic <milostanaskovic.dev.data@gmail.com>
2024-02-29 14:35:30 +0100

    implement invert functionality for periodic table

--- src/designsystem/atoms/buttons/ButtonOutline.tsx
+++ src/designsystem/atoms/buttons/ButtonOutline.tsx
@@ -3,14 +3,17 @@ import React from "react";
 interface ButtonOutlineProps {
   children: React.ReactNode;
   className?: string;
+  handleBtnClick?: () => void;
 }
 
 const ButtonOutline: React.FC<ButtonOutlineProps> = ({
   children,
   className,
+  handleBtnClick,
 }) => (
   <button
     className={`bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded ${className}`}
+    onClick={handleBtnClick}
   >
     {children}
   </button>
