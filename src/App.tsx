35e6606aa4b2b164676f11c215b2bfb6a3050315
MilosTanaskovic <milostanaskovic.dev.data@gmail.com>
2024-03-01 11:21:44 +0100

    implement revert, toglle (invert - revert btn)

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <div className=" flex flex-col gap-24">
       {/* <Header /> */}
-      <Header onInvertClick={handleInvertClick} />
+      <Header isInverted={isInverted} onInvertClick={handleInvertClick} />
       {/* Periodic Table */}
       <PeriodicTable isInverted={isInverted} />
       {/* <PeriodicTableTest /> */}
